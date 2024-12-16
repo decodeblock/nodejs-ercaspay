aadd2500b34fa7c9ec40fba7344b5a633d2575a3
Gabriel Ibenye <gabrielibenye@gmail.com>
2024-12-16 08:37:53 -0500

    added static value for language

--- src/Types/BrowserDetails.js
+++ src/Types/BrowserDetails.js
@@ -22,17 +22,16 @@ class BrowserDetails {
   // Static factory method (can handle Express's req or Koa's ctx.request)
   static fromRequest(request) {
     const headers = request.headers || {};
-    const acceptLanguage = headers['accept-language'] || 'en-US';
 
     return new BrowserDetails({
-      challengeWindowSize: 'FULL_SCREEN', // Default value, could be dynamically set
+      challengeWindowSize: 'FULL_SCREEN',
       acceptHeaders: headers.accept || 'application/json',
-      colorDepth: 24, // Static value, might be dynamically set based on user agent or other data
-      javaEnabled: true, // Example value, could be detected if necessary
-      language: acceptLanguage,
-      screenHeight: 473, // Can be dynamically calculated if necessary
-      screenWidth: 1600, // Can be dynamically calculated if necessary
-      timeZone: 273, // Default time zone, can be dynamically calculated
+      colorDepth: 24,
+      javaEnabled: true,
+      language: 'en-US',
+      screenHeight: 473,
+      screenWidth: 1600,
+      timeZone: 273,
     });
   }
 
